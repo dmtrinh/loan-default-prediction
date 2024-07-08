90d2e28ec008125f289009f667232c31d2042fdb
dmtrinh <1691596+dmtrinh@users.noreply.github.com>
2024-07-07 21:46:03 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# loan-default-prediction
+# Default prediction to optimize loan portfolio performance
+
+## Introduction
+LendingClub is an early peer-to-peer lending network for borrowers seeking loans between $1,000 to $40,000.  These loans are funded by investors who are looking for higher-than-normal returns on their capital.  Since these loans are unsecured, investors need to perform their own due diligence to minimize loss of original investment and maximize returns.
+
+To help investors in the decisioning process, LendingClub makes available many borrower and loan-specific information.  Borrower information include demographic and economic details such as location, employment, income, credit scores, and more.  In addition, information on interest rate, term duration, and purpose are provided for each loan.
+
+The goal of this project is to evaluate several ML classification models to determine their effectiveness in predicting whether a LendingClub loan will be fully paid.  Investors can use these predictions to optimize their capital allocations for maximum profit.
+
+## Rationale
+Optimizing loan portfolio performance is a fundamental use case for banks and lenders.  Classification models play an important role by helping to predict default risk for a borrower -- enabling lenders to minimize loan losses through better decisions on loan funding.
+
+## Data Sources
+The dataset contains over 2.8 million loan records on the LendingClub platform spanning 2007 through Q3 2020.  It is available for download from [Kaggle](https://www.kaggle.com/datasets/ethon0426/lending-club-20072020q1)
+
+## Methodology
+
+We will employ many phases of [CRISP-DM](https://en.wikipedia.org/wiki/Cross-industry_standard_process_for_data_mining) in this project to explore, clean, and prepare the data for modeling.
+
+## Data Preprocessing and Exploratory Data Analysis
+The LendingClub dataset is high dimensional with 142 features.
+
+After reviewing [LendingClub's companion data dictionary](./data/LCDataDictionary.xlsx), the following features were pruned:
+* Features with high percentage of missing values:
+![Features with high % of missing values](./output/table_features_with_over_40_pct_missing_values_.png)
+* Features where data would only be available after the loan has been funded.  Obviously, these would not be useful since our goal is to help determine whether or not the loan should be funded in the first place.
+* Features where data was not captured prior to `2012-08-01`
+* `id`, `member_id`, and `url`.  These are useful as indices, but not useful for our model.
+* `desc`.  This information is formally categorized in loan `purpose` by LendingClub.
+
+
+A breakdown of loans by statuses:
+![Loan Status Distribution](./output/loan_status_distribution.png)
+
---
+++ helper_functions.py
@@ -0,0 +1,123 @@
+#import pandas as pd
+#import numpy as np
+#import matplotlib.pyplot as plt
+
+# Define a function to print all unique values for each column
+def print_unique_values(df):
+    for col in df.columns:
+        # if count of unique values is less than 20, print the unique values
+        if len(df[col].unique()) < 20:
+            print(f'{col}: {df[col].unique()}')
+
+# Define function to print the percentage of loan_status for each value in a column
+def print_loan_status_percentage(df, column_name):
+    for i in df[column_name].unique():
+        if (i < 10):
+            print(f'{column_name} = {i}')
+            print(df[df[column_name] == i]['loan_status'].value_counts(normalize=True))
+            print('----')
+
+# Function to create a correlation matrix
+def plot_corr(df, size=10):
+    """
+    Function plots a graphical correlation matrix for each pair of columns in the dataframe.
+
+    Input:
+        df: pandas DataFrame
+        size: vertical and horizontal size of the plot
+
+    Displays:
+        matrix of correlation between columns.  Blue-cyan-yellow-red-darkred => less to more correlated
+                                                0 ------------------>  1
+                                                Expect a darkred line running from top left to bottom right
+    """
+
+    corr = df.corr()
+    fig, ax = plt.subplots(figsize=(size, size))
+    cax = ax.matshow(corr, cmap='coolwarm')
+    fig.colorbar(cax)
+    plt.xticks(range(len(corr.columns)), corr.columns, rotation=90)
+    plt.yticks(range(len(corr.columns)), corr.columns)
+    plt.savefig('./output/correlation_matrix.png', dpi=300, bbox_inches='tight')
+    plt.show()
+    
+def create_bins(df, col, n_bins):
+    """
+    Function to create bins for a given column in the dataframe
+
+    Input:
+        df: pandas DataFrame
+        col: column name to create bins for
+        n_bins: number of bins to create
+
+    Returns:
+        df: pandas DataFrame with a new column that contains the bin number for each row
+    """
+
+    # if col is of type 'object', attempt to convert it to a numeric type
+    if df[col].dtype == 'object':
+        print(f'Column {col} is of type object')
+        try:
+            df[col] = pd.to_numeric(df[col])
+        except:
+            print(f'Column {col} is not numeric')
+            try:
+                # Attempt to parse percentage values
+                print(f'Attempting to convert {col} to numeric')
+                df[col] = df[col].str.rstrip('%').astype('float64')
+            except:
+                print(f'Unable to convert {col} to numeric')
+                return df
+
+    
+    # Create a new column with the column name + '_bin'
+    new_col = col + '_bin'
+    # Create bins for the column
+    df[new_col] = pd.cut(df[col], bins=n_bins)
+    return df
+
+def create_wordcloud(input: pd.Series, max_words: int = 1000):
+    from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+    print(f'Number of unique values: {input.nunique()}')
+    
+    # replace space in values with underscore
+    input = input.str.replace(' ', '_')
+
+    # create string out of all values in input series
+    text = ' '.join(input.dropna())
+
+    wordcloud = WordCloud(
+        width=1920, 
+        height=1080, 
+        max_words=max_words, 
+        random_state=42, 
+#        colormap='cividis'
+#        colormap='plasma'
+        colormap='cool'
+    ).generate(text)
+
+    # Generate plot
+    plt.figure(figsize=(12, 8))
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    plt.savefig('./output/wordcloud_' + input.name + '.png', dpi=300, bbox_inches='tight')
+    plt.show()
+
+def plot_dataframe_as_table_image(df, table_name, index_title):
+    """
+    Function to plot a dataframe as an image
+
+    Input:
+        df: pandas DataFrame
+        title: title of the plot
+    """
+
+    fig = ff.create_table(df, index=True, index_title=index_title)
+    fig.update_layout(
+        autosize=True,
+        width=800
+    )
+
+    fig.write_image('./output/table_' + table_name + '_.png', scale=2)
+    fig.show()
